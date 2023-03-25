13b7a27f1e5b8b38c6d278b0c7f931bf34ebc1d0
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-03-25 22:02:23 +0300

    commit tree cdd237d4c76098e67e8297e7889f27c70736e733

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,44 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarfnames)
+  dwarves = []
+  dwarfnames.each_with_index {|dwarfname,index|
+  dwarves << "#{index+1}.#{dwarfname}"}
+  return puts dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+puts roll_call_dwarves(["Dopey","Doc","Bashful","Grumpy","Happy","Sleepy","Sneezy"])
+
+#----------------------------------------------------------------------
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|planet| "#{planet.capitalize}!"}
+end
+puts summon_captain_planet(["earth","wind","fire","water","heart"])
+
+#----------------------------------------------------------------------
+
+def long_planeteer_calls(arrays)
+ if arrays.any? {|arr| arr.length > 4}
+    return true
+    else 
+    return false
+  end
+end
+puts long_planeteer_calls(["wind", "fire", "tree", "code"])
+puts long_planeteer_calls(["earth", "wind", "fire"])
+
+#----------------------------------------------------------------------
+
+def find_the_cheese(found)
+  cheese_types=["cheddar", "gouda", "camembert"]
+
+  if found.find {|cheddar|cheese_types.include?(cheddar)}
+    return "cheddar"
+    else 
+    return nil
+    end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+puts find_the_cheese([["banana", "cheddar", "sock"]])
+puts find_the_cheese(["ham", "cellphone", "computer"])
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+#----------------------------------------------------------------------
